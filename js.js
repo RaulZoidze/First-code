4ad5a6b3ce1be36e03459884d5b7467d7d5c03fa
RaulZoidze <Raulzoidze3301@gmail.com>
2019-08-25 13:14:17 +0300

    i create lesson_01

---
+++ js.js
@@ -0,0 +1,25 @@
+﻿alert ("Hello Glo academy!");
+
+console.log('hello world') 
+
+let  today = new Date(),
+     hourNow = today.getHours()
+     greeting;
+
+    if ( hourNow > 18 )  {
+        greeting = 'Добрый Вечер Glo academy! ';
+    }  else if ( hourNow > 12 ) {
+        greeting = 'Добрый День Glo academy!' ;
+    }  else if ( hourNow > 0 ) {
+        greeting = 'Доброе утро Glo academy!' ;
+    }   else {
+        greeting = 'Hello world Glo academy!'
+    }
+
+    alert ( greeting ) ;
+
+    console.log ( greeting );
+
+
+
+
