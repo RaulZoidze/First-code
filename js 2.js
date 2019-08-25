8d65ba1e898ecd76cc3579885c9d02f88a08dd2c
RaulZoidze <Raulzoidze3301@gmail.com>
2019-08-25 13:47:24 +0300

    i create lesson_02

---
+++ js 2.js
@@ -0,0 +1,26 @@
+let     money = 100000,
+        income = 500000 + ' $' +' такси :D', 
+        addExpenses = 20000 + ' Квартира' + ',' + ' 50000 Машина' + ',' + '100000000 Лецензия П.О' ,
+        deposit = true,
+        mission = 9999999999999 + '$',
+        period = 12,
+        budgetDay = money;
+        
+     console.log (typeof money)
+     console.log (money +'$' + ' доход за месяц') 
+
+     console.log (typeof income)
+     console.log (income.length)
+
+     console.log (typeof addExpenses)
+     console.log (addExpenses.toLowerCase)
+     console.log (addExpenses.split(' '))
+     console.log ('Цель Заработать ' + (mission))
+     console.log ('Период ' + (period) + ' Месяцев')
+     console.log (budgetDay / 30)
+     console.log(budgetDay % 30)
+    
+
+
+
+
