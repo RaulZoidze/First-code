268fa72d057e83e626b0cd2abab3ca631b8b78f3
RaulZoidze <Raulzoidze3301@gmail.com>
2019-08-29 13:26:41 +0300

    i create lesson_05

--- js.js
+++ js.js
@@ -1,68 +1,79 @@
 'use strict'
-let     money= prompt ("Ваш месячный доход?") ,
+let     money ,
         addExpenses = prompt("Перечислите возможные расходы за рассчитываемый период через запятую") ,
-        costsFirst1= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
-        costsFirst2= prompt ("Во сколько это обойдется?") ,
-        costsFirst3= prompt ("Какие обязательные ежемесячные расходы у вас есть?") ,
-        costsFirst4= prompt ("Во сколько это обойдется?") ,
-        income = 500, 
+        costsFirst1,
+        costsFirst3,
+        income = "фриланс", 
         deposit =confirm("Есть ли у вас депозит в банке?") ,
         mission = 10000,
         period = 6,
         budgetMonth,
         budgetDay;  
 
-       
 
-
-
-        console.log("месячный доход :" + Number(money));
-        console.log (addExpenses.split(' '));
-
-        console.log("депозит в банке :" + deposit);
-       // console.log (typeof money);
-       // console.log (typeof income);
-       // console.log (typeof deposit);
-        console.log( "обязательные расходы :" + costsFirst1);
-        console.log( "стоимость обязательных расходов :" + Number(costsFirst2));
-        console.log( "обязательные расходы :" + costsFirst3);
-        console.log( "стоимость обязательных расходов :" + Number(costsFirst4));
-
-       // budgetMonth = money - costsFirst2 - costsFirst4;
+let     start = function () {
+          do {
+          money= +prompt ("Ваш месячный доход?",50000 );
+         }
         
-       // budgetDay = budgetMonth / 30;
-        
+          while(isNaN (money) || money === '' || money === null) {}
+};
+        start();
 
-       // console.log("месячный бюджет :" + Number(budgetMonth));
-       // console.log("сколько до цели :" + Math.ceil(Number(budgetMonth / mission)));
-       // console.log("дневной бюджет :" + Math.floor(budgetDay));
 
-       
+        // возвращаем обязательные расходы
+        let getExpensesMonth = function () {
+          let sum=0;
 
-        let sum ;
-            function getExpensesMonth () {
-                sum = Number(costsFirst2) + Number(costsFirst4); 
+          
+
+          for (let i = 0; i < 2;i++) {
+            if (i === 0) {
+              costsFirst1= prompt ("Какие обязательные ежемесячные расходы у вас есть?");
+            }
+            if (i === 1) {
+              costsFirst3= prompt ("Какие обязательные ежемесячные расходы у вас есть?");
+            }
+          }
+          do { 
+            sum += +prompt ("Во сколько это обойдется?")
+            sum += +prompt ("Во сколько это обойдется?")
+          }
+          while (isNaN (sum) || sum === null ) {};
+        return sum;
+          
         }
-            getExpensesMonth(costsFirst2 + costsFirst4);
-            console.log("сумма обязательных расходов :" + sum);
-        
+          let expensesAmount = getExpensesMonth();
+            console.log("сумма обязательных расходов :" + expensesAmount);
+
+
+
+          //возвращаем накопления за месяц
         let accumulatedMonth ;
-            function getAccumulatedMonth () {
-                accumulatedMonth = Number(money) + Number(income) - Number(sum);
+
+        function getAccumulatedMonth () {
+          accumulatedMonth = money - expensesAmount;
                 
           }
-          getAccumulatedMonth(money+income - sum);
+          getAccumulatedMonth();
           console.log("накопления за месяц :" + accumulatedMonth);
 
         let getMonth ;
-          function getTargetMonth () {
-            getMonth = Number(mission) / Number(accumulatedMonth);
+            //возвращаем период до цели
+
+        function getTargetMonth () {
+          getMonth = mission / accumulatedMonth;
+          if (getMonth < 0) {
+            console.log("цель не будет достигнута ")
+          }
+          else {
+            console.log("цель будет достигнута за  :" + Math.floor(getMonth) + " месяца");}
 
           }
-          getTargetMonth(mission / accumulatedMonth);
-          console.log("за какой период будет достигнута цель :" + Math.floor(getMonth) + " месяца");
+          getTargetMonth();
+          
 
-
+            //возвращаем тип данных
         let showTypeOf = function (data) {
             console.log(data,typeof(data));
             
@@ -71,17 +82,47 @@ let     money= prompt ("Ваш месячный доход?") ,
         showTypeOf(income);
         showTypeOf(deposit);
 
-
+          //возвращаем уровень дохода
         let  getStatusIncome = function () {
 
-        if (budgetDay <= 800){  
+        if (budgetMonth >= 800){  
            return  ("Высокий уровень дохода")  ;     
         }
-        else if (budgetDay <= 300 && budgetDay >= 800 ) {
+        else if (budgetMonth <= 300 && budgetDay >= 800 ) {
            return  ("Средний уровень дохода") ;
         }
         else  {
            return  ("Что то пошло не так");
         }
         };
-            console.log(getStatusIncome());
+
+        //Дневной бюджет
+            budgetMonth = money - expensesAmount;
+            budgetDay = budgetMonth / 30; 
+
+        if (budgetDay < 0  ) {
+          console.log("дневной бюджет :Что-то пошло не так")
+        }
+        else {
+          console.log("дневной бюджет :" + Math.floor(budgetDay));
+        };
+
+            //лог
+            console.log(getStatusIncome()); 
+            console.log("месячный доход :" + +money);
+            console.log (addExpenses.split(' '));
+    
+            console.log("депозит в банке :" + deposit);
+           
+            /*console.log( "обязательные расходы :" + costsFirst1);
+            console.log( "стоимость обязательных расходов :" + Number(costsFirst2));
+            console.log( "обязательные расходы :" + costsFirst3);
+            console.log( "стоимость обязательных расходов :" + Number(costsFirst4));*/
+    
+            
+            
+    
+           // console.log("месячный бюджет :" + Number(budgetMonth));
+            //console.log("сколько до цели :" + Math.ceil(Number(budgetMonth / mission)));
+           
+    
